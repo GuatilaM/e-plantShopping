bdb9835fd2fcba827e6f59864b39f5712adb9fcf
guatilaM <guatilam@gmail.com>
2025-06-23 20:29:47 -0500

    feat: shows products and adds functionality to add and remove from cart
    displays the products in cards and calculates total amounts of items and cost

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,33 +3,46 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
+// eslint-disable-next-line react/prop-types
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    if (cart.length > 0){
+      let totalAmount = cart.reduce((total, item) => total + (item.quantity * parseFloat(item.cost.substring(1))), 0);
+      return totalAmount;
+    } else {return 0}
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e); 
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity <= 1){
+      dispatch(removeItem(item));
+    } else {
+      dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1));
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,17 +7,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const itemExists = state.items.find((item) => item.name === action.payload.name);
+      if (!itemExists){
+        state.items.push({...action.payload, quantity: 1});
+      } else {
+        itemExists.quantity++;
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const itemExists = state.items.find((item) => item.name === action.payload.name);
+      if (itemExists){
+        itemExists.quantity = action.payload.quantity;
+      }
     },
   },
 });
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
 export default CartSlice.reducer;
